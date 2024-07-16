a9309f91f1893a0e2422b279c8f3e2f41c5aad36
YoutechA320U <42425276+YoutechA320U@users.noreply.github.com>
2024-07-16 22:54:39 +0900

    Update ELYZA-tasks100-ansuwer_karakuri.py

--- ELYZA-tasks100-ansuwer_karakuri.py
+++ ELYZA-tasks100-ansuwer_karakuri.py
@@ -47,8 +47,7 @@ with open(csv_path, mode='r', encoding='utf-8',newline="") as file:
             stop=["<|","prompt_tokens"] # ストップ。特定の文字を生成したらその文字を生成せず停止する。
         )
         output= output["choices"][0]["text"]
-        output =output.replace("\\n", "\n").replace("\\n", "\n").replace("\\u3000", "\u3000")\
-            .replace("!","！").replace("?","？")
+        output =output.replace("\\n", "\n").replace("\\u3000", "\u3000").replace("!","！").replace("?","？")
         while output[-1]=="\n":
               output=output[:-1]
         while output[0]=="\n":

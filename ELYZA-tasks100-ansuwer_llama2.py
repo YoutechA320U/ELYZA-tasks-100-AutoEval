245e432c56999db2c5b8a0d896a7b987ce05d592
YoutechA320U <42425276+YoutechA320U@users.noreply.github.com>
2024-07-18 22:43:26 +0900

    コンテキストサイズと温度を修正

--- ELYZA-tasks100-ansuwer_llama2.py
+++ ELYZA-tasks100-ansuwer_llama2.py
@@ -4,7 +4,7 @@ model="hoge_huga.gguf" #評価対象のモデルのパスを入力。
 llm = Llama(
       model_path=model,
       n_gpu_layers=-1, # #GPUにロードするレイヤー数（llama-cpp-pythonがcuda版の場合）
-      n_ctx=4096, # 最大コンテキストサイズ。入力の上限。
+      n_ctx=2048, # 最大コンテキストサイズ。入力の上限。
       #last_n_tokens_size =0, # Maximum number of tokens to keep in the last_n_tokens deque.
 )
 
@@ -33,7 +33,7 @@ with open(csv_path, mode='r', encoding='utf-8',newline="") as file:
         output = llm(
             prompt=prompt_L2,
             max_tokens=1024,
-            temperature = 0.7,
+            temperature = 0.8,
             top_p=0.95, 
             min_p=0.05,
             typical_p=1.0,
@@ -46,7 +46,7 @@ with open(csv_path, mode='r', encoding='utf-8',newline="") as file:
             mirostat_mode=0,
             mirostat_tau=5.0,
             mirostat_eta=0.1,
-            stop=["<</SYS>>","[INST]","[/INST]","</SYS>","prompt_tokens"] # ストップ。特定の文字を生成したらその文字を生成せず停止する。
+            stop=["<</SYS>>","[INST]","[/INST]","</SYS>"] # ストップ。特定の文字を生成したらその文字を生成せず停止する。
         )
         output= output["choices"][0]["text"]
         output =output.replace("\\n", "\n").replace("\\u3000", "\u3000").replace("!","！").replace("?","？")
